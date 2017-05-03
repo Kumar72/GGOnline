05b24e3a395a0198233a122b0801caa90e76335e
StefanFuller147 <sfullersywn@gmail.com>
2017-05-03 13:19:17 -0600

    updated daoImpls

--- GGOREST/src/data/GameDAOImpl.java
+++ GGOREST/src/data/GameDAOImpl.java
@@ -2,20 +2,34 @@ package data;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import entities.Game;
+@Transactional
 
 public class GameDAOImpl implements GameDAO{
+	
+	@PersistenceContext
+	private EntityManager em;
 
+	
+	
 	@Override
 	public List<Game> index() {
-		// TODO Auto-generated method stub
-		return null;
+		String q = "SELECT g FROM Game g";
+		List<Game> game = em.createQuery(q, Game.class).getResultList();
+		
+		return game;
+		
 	}
 
 	@Override
 	public Game show(int gameId) {
-		// TODO Auto-generated method stub
-		return null;
+		Game game = em.find(Game.class, gameId);
+		return game;
 	}
 
 }
--- GGOREST/src/data/RatingDAO.java
+++ GGOREST/src/data/RatingDAO.java
@@ -1,5 +1,10 @@
 package data;
 
+import java.util.List;
+
+import entities.Rating;
+
 public interface RatingDAO {
+	public List<Rating> index(int playerId);
 
 }
