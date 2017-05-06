8de6f2426c9cdab923492f3afe5e3870602ead07
Kumar72 <vichandan@gmail.com>
2017-05-05 19:09:34 -0600

    css stuff

--- GGOREST/WebContent/app/appModule/appComponent/app.component.js
+++ GGOREST/WebContent/app/appModule/appComponent/app.component.js
@@ -1,3 +1,18 @@
 angular.module("appModule").component("appComponent", {
-    templateUrl : 'app/appModule/appComponent/app.component.html'
+    templateUrl : 'app/appModule/appComponent/app.component.html',
+    controller: function(authService, $location) {
+    	var vm = this;
+    	vm.logIn = function() {
+    		if(authService.getToken().id){
+    			vm.name = authService.getToken().fname;	
+    			document.body.style.backgroundImage = "url('http://wallpaper-gallery.net/images/desktop-wallpaper-gaming/desktop-wallpaper-gaming-6.jpg')";
+    			return true
+    			
+    		}
+    		else{
+    		return false;
+    		}
+    	}
+    },
+    controllerAs: 'vm'
     });
