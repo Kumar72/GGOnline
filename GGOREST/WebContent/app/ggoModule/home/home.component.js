c3e845c626293a9a8b212e557bf77112e773710b
Kumar72 <vichandan@gmail.com>
2017-05-07 11:25:14 -0600

    team form added

--- GGOREST/WebContent/app/ggoModule/home/home.component.js
+++ GGOREST/WebContent/app/ggoModule/home/home.component.js
@@ -1,11 +1,12 @@
 angular.module("ggoModule").component("home", {
     templateUrl : 'app/ggoModule/home/home.component.html',
-    controller: function(ggoService, $location) {
+    controller: function(ggoService, $location, $scope) {
     	var vm = this;
     	
     	vm.player =[];
     	vm.games = [];
     	vm.teams = [];
+    	vm.showTeamForm = true;
     	
     	vm.reload = function(){
     		ggoService.showUser().then(function(res){
@@ -17,20 +18,36 @@ angular.module("ggoModule").component("home", {
         		vm.games = res.data;
     	    })
     	    
-    	    ggoService.playerTeams().then(function(res){
-        		vm.teams = res.data;
-    	    })
-    	    
     	    vm.removeGame = function(game){
     			ggoService.removeGame(game)
     				.then(console.log)
     				.catch(console.error)
-    			
     		}
+    	    
+    	    ggoService.playerTeams().then(function(res){
+        		vm.teams = res.data;
+    	    })
     	}
     	
-    	
     	vm.reload();
+    	
+    	vm.displayForm = function() {
+    		vm.showTeamForm = false;
+    	}
+    	vm.cancelButton = function() {
+    		vm.showTeamForm = true;
+    	}
+    	
+    	vm.modalShown = false;
+    	  vm.toggleModal = function() {
+    		  console.log("in toggleModal function")
+    	    vm.modalShown = !vm.modalShown;
+    	  };
+    	
+    	vm.displayGameInfo = function() {}
+    	
+    	
+    	
     	
     	
     	
